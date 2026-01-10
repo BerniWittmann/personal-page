c7692bf879f42df0716fd6505a89174566cf7b24
Bernhard Wittmann <dev+github@bernhardwittmann.com>
2026-01-10 12:23:17 +0100

    perf: :zap: add playform-compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,15 @@ import sitemap from '@astrojs/sitemap';
 
 import swup from '@swup/astro';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bernhardwittmann.com',
-  integrations: [mdx(), sitemap(), swup({ globalInstance: true })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    swup({ globalInstance: true }),
+    playformCompress(),
+  ],
 });
