f7a8f4f9ab285c0b1c0d358bba021a2bd3fcbd69
Bernhard Wittmann <dev+github@bernhardwittmann.com>
2026-01-10 11:51:52 +0100

    feat: :sparkles: add swup for animating page transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import swup from '@swup/astro';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bernhardwittmann.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), swup({ globalInstance: true })],
 });
